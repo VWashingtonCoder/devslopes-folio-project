84b00c0be323be194e33f096c54dc02b00765cef
V. Andre Washington <kuraiokamiblaze@gmail.com>
2022-12-31 15:22:14 -0700

    Add carousel javascript
    Added logic to update slides and css

--- js/carouse.l.js
+++ js/carouse.l.js
@@ -5,30 +5,20 @@ let current = Math.floor(Math.random() * slides.length);
 let next = current < slides.length - 1 ? current + 1 : 0;
 let prev = current > 0 ? current - 1 : slides.length - 1;
 
-const dummySlides = [
-    // slide 0
-    // slide 1 - prev
-    // slide 2 - current [next] = current + 1
-    // slide 3 - current 
-]
-
-/* create eventListener for prev/next  */
-// goToNext()
-// goToPrev()
-// updateIndexes(param)
-// updateCSS();
-
-// -- decide how to call prev/next
-// -- update variables
-// --- [current] = newIndex
-// --- [next] = current + 1 : 0
-// --- [prev] = current - 1 : length - 1
-// update the css
+const updateSlides = () => {
+    slides.forEach((slide) => {
+        slide.classList.remove('active', 'prev', 'next');
+    })
+    slides[current].classList.add('active');
+    slides[prev].classList.add('prev');
+    slides[next].classList.add('next');
+}
 
 const goToIndex = (idx) => {
     current = idx;
     next = idx < slides.length - 1 ? idx + 1 : 0;
     prev = idx > 0 ? idx - 1 : slides.length - 1;
+    updateSlides();
 }
 
 const goToNext = () => current < slides.length - 1 ? goToIndex(current + 1) : goToIndex(0);
@@ -38,3 +28,4 @@ for (let i = 0; i < buttons.length; i += 1) {
     buttons[i].addEventListener('click', () => i === 0 ? goToPrev() : goToNext());
 }
 
+updateSlides();
